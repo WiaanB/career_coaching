5d6f2e1b9bf9ba4924aa723bfac85f1b43f253dc
wiaan <whoppingwaki@gmail.com>
2022-10-07 07:21:32 +0200

    implemented basic number slice generation

---
+++ binary_search/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/WiaanB/career_coaching/utils"
+)
+
+func main() {
+	s := utils.GenerateNumSlice(10, 0, 10)
+	fmt.Println(s)
+}
--- utils/numbers.go
+++ utils/numbers.go
@@ -2,7 +2,7 @@ package utils
 
 import "math/rand"
 
-func Generate(length, lower, upper int) (slice []int) {
+func GenerateNumSlice(length, lower, upper int) (slice []int) {
 	slice = make([]int, length)
 	for i := 0; i < length; i++ {
 		slice[i] = rand.Intn((upper - lower) + lower)
