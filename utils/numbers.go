4d51ccf47aba4fc6bf38c7320fb46d52083dec76
wiaan <whoppingwaki@gmail.com>
2022-10-07 07:58:06 +0200

    Adjusted the numbers util functionality

--- utils/numbers.go
+++ utils/numbers.go
@@ -2,10 +2,18 @@ package utils
 
 import "math/rand"
 
-func GenerateNumSlice(length, lower, upper int) (slice []int) {
+func GenerateRandomNumSlice(length, lower, upper int) (slice []int) {
 	slice = make([]int, length)
 	for i := 0; i < length; i++ {
 		slice[i] = rand.Intn((upper - lower) + lower)
+	}
+	return
+}
+
+func GenerateSortedSlice(length int) (slice []int) {
+	slice = make([]int, length)
+	for i := 0; i < length; i++ {
+		slice[i] = i
 	}
 	return
 }
