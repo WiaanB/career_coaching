e3e10471361b8a325965e050cd913dc01f2e11f9
wiaan <whoppingwaki@gmail.com>
2022-10-07 07:58:22 +0200

    implemented a binary search

--- binary_search/main.go
+++ binary_search/main.go
@@ -2,11 +2,41 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/WiaanB/career_coaching/utils"
+	"sort"
 )
 
 func main() {
-	s := utils.GenerateNumSlice(10, 0, 10)
-	fmt.Println(s)
+	short := []int{0, 6, 11, 11, 18, 25, 28, 37, 40, 45, 47, 56, 59, 62, 74, 81, 81, 87, 89, 94}
+	fmt.Println(binarySearch(short, 40, 0, len(short)))
+}
+
+func sortSlice(s []int, direction string) []int {
+	if direction == "asc" {
+		sort.Slice(s, func(i, j int) bool {
+			return s[i] < s[j]
+		})
+	} else {
+		sort.Slice(s, func(i, j int) bool {
+			return s[i] > s[j]
+		})
+	}
+	return s
+}
+
+func binarySearch(s []int, target, low, high int) int {
+	val := 0
+	for low != high {
+		mid := (low + high) / 2
+		if s[mid] == target {
+			val = mid
+			break
+		}
+
+		if s[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return val
 }
